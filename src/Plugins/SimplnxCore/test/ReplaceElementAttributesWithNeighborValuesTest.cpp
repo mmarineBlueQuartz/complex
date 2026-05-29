a743e146bcb99b4b2100ce8867d4e2b6d72cd7aa
Matthew Marine <matthew.marine@bluequartz.net>
2026-05-29 12:59:31 -0400

    Add ReplaceElementAttributeWithNeighborValues unit test
    * No confidence
    * Less Than comparison
    * Greater than comparison
    * Loop / No loop

--- src/Plugins/SimplnxCore/test/ReplaceElementAttributesWithNeighborValuesTest.cpp
+++ src/Plugins/SimplnxCore/test/ReplaceElementAttributesWithNeighborValuesTest.cpp
@@ -4,6 +4,8 @@
 #include "SimplnxCore/Filters/ReplaceElementAttributesWithNeighborValuesFilter.hpp"
 
 #include "simplnx/Core/Application.hpp"
+#include "simplnx/DataStructure/DataArray.hpp"
+#include "simplnx/DataStructure/DataStore.hpp"
 #include "simplnx/Parameters/ChoicesParameter.hpp"
 #include "simplnx/Pipeline/Pipeline.hpp"
 #include "simplnx/Pipeline/PipelineFilter.hpp"
@@ -11,6 +13,8 @@
 
 #include <filesystem>
 #include <fstream>
+#include <memory>
+
 namespace fs = std::filesystem;
 
 using namespace nx::core;
@@ -21,7 +25,223 @@ namespace
 {
 const DataPath k_ConfidenceIndexPath = k_CellAttributeMatrix.createChildPath(Constants::k_Confidence_Index);
 const std::string k_ExemplarDataContainer2("DataContainer");
+
+constexpr StringLiteral k_Data = "Data";
+const DataPath k_DataPath({k_ImageGeometry, k_Data});
+const ShapeType k_Dimensions = {2, 3, 1};
 } // namespace
+
+DataStructure CreateTestData()
+{
+  DataStructure dataStructure;
+
+  auto* imageGeom = ImageGeom::Create(dataStructure, ::k_ImageGeometry);
+  imageGeom->setDimensions(::k_Dimensions);
+
+  auto store = std::make_shared<DataStore<float32>>(k_Dimensions, ShapeType{1}, 0);
+  auto* dataArrayPtr = DataArray<float32>::Create(dataStructure, k_Data, store, imageGeom->getId());
+  auto& storeRef = *store.get();
+
+  for(usize i = 0; i < 3; i++)
+  {
+    storeRef[i * 2 + 0] = 0.010f * i;
+    storeRef[i * 2 + 1] = 0.012f * i;
+  }
+  storeRef[5] = 10.0;
+
+  return dataStructure;
+}
+
+void CheckTest1Output(DataStructure& dataStructure)
+{
+  auto& dataStore = dataStructure.getDataRefAs<DataArray<float32>>(k_DataPath).getDataStoreRef();
+  REQUIRE(dataStore[0] == 0.0f);
+  REQUIRE(dataStore[1] == 0.0f);
+  REQUIRE(dataStore[2] == 0.010f);
+  REQUIRE(dataStore[3] == 0.012f);
+  REQUIRE(dataStore[4] == 0.020f);
+  REQUIRE(dataStore[5] == 10.0f);
+}
+
+// Less than comparison
+void CheckTest2Output(DataStructure& dataStructure)
+{
+  auto& dataStore = dataStructure.getDataRefAs<DataArray<float32>>(k_DataPath).getDataStoreRef();
+  REQUIRE(dataStore[0] == 0.0f);
+  REQUIRE(dataStore[1] == 0.0f);
+  REQUIRE(dataStore[2] == 0.010f);
+  REQUIRE(dataStore[3] == 0.010f);
+  REQUIRE(dataStore[4] == 0.020f);
+  REQUIRE(dataStore[5] == 10.0f);
+}
+
+// Greater than comparison
+void CheckTest3Output(DataStructure& dataStructure)
+{
+  auto& dataStore = dataStructure.getDataRefAs<DataArray<float32>>(k_DataPath).getDataStoreRef();
+  REQUIRE(dataStore[0] == 0.0f);
+  REQUIRE(dataStore[1] == 0.010f);
+  REQUIRE(dataStore[2] == 0.012f);
+  REQUIRE(dataStore[3] == 0.020f);
+  REQUIRE(dataStore[4] == 0.020f);
+  REQUIRE(dataStore[5] == 10.0f);
+}
+
+// Loop Less Than: Loop
+void CheckTest4Output(DataStructure& dataStructure)
+{
+  auto& dataStore = dataStructure.getDataRefAs<DataArray<float32>>(k_DataPath).getDataStoreRef();
+  REQUIRE(dataStore[0] == 0.0f);
+  REQUIRE(dataStore[1] == 0.0f);
+  REQUIRE(dataStore[2] == 0.010f);
+  REQUIRE(dataStore[3] == 0.010f);
+  REQUIRE(dataStore[4] == 0.020f);
+  REQUIRE(dataStore[5] == 10.0f);
+}
+
+// Loop Greater Than: Loop
+void CheckTest5Output(DataStructure& dataStructure)
+{
+  auto& dataStore = dataStructure.getDataRefAs<DataArray<float32>>(k_DataPath).getDataStoreRef();
+  REQUIRE(dataStore[0] == 0.0f);
+  REQUIRE(dataStore[1] == 0.0f);
+  REQUIRE(dataStore[2] == 0.012f);
+  REQUIRE(dataStore[3] == 0.012f);
+  REQUIRE(dataStore[4] == 0.024f);
+  REQUIRE(dataStore[5] == 10.0f);
+}
+
+// Loop Less Than: Loop: Value 2
+void CheckTest6Output(DataStructure& dataStructure)
+{
+  auto& dataStore = dataStructure.getDataRefAs<DataArray<float32>>(k_DataPath).getDataStoreRef();
+  REQUIRE(dataStore[0] == 0.0f);
+  REQUIRE(dataStore[1] == 0.0f);
+  REQUIRE(dataStore[2] == 0.0f);
+  REQUIRE(dataStore[3] == 0.0f);
+  REQUIRE(dataStore[4] == 0.0f);
+  REQUIRE(dataStore[5] == 10.0f);
+}
+
+// Loop Less Than: Loop: Value 2
+void CheckTest7Output(DataStructure& dataStructure)
+{
+  auto& dataStore = dataStructure.getDataRefAs<DataArray<float32>>(k_DataPath).getDataStoreRef();
+  REQUIRE(dataStore[0] == 0.024f);
+  REQUIRE(dataStore[1] == 0.024f);
+  REQUIRE(dataStore[2] == 0.024f);
+  REQUIRE(dataStore[3] == 0.024f);
+  REQUIRE(dataStore[4] == 0.024f);
+  REQUIRE(dataStore[5] == 10.0f);
+}
+
+void RunFilter(DataStructure& dataStructure, uint64 comparisonType, float32 minConfidence, bool loop)
+{
+  {
+    // Instantiate the filter, a DataStructure object and an Arguments Object
+    ReplaceElementAttributesWithNeighborValuesFilter filter;
+    Arguments args;
+
+    // Create default Parameters for the filter.
+    args.insertOrAssign(ReplaceElementAttributesWithNeighborValuesFilter::k_MinConfidence_Key, std::make_any<float32>(minConfidence));
+    args.insertOrAssign(ReplaceElementAttributesWithNeighborValuesFilter::k_SelectedComparison_Key, std::make_any<ChoicesParameter::ValueType>(comparisonType));
+    args.insertOrAssign(ReplaceElementAttributesWithNeighborValuesFilter::k_Loop_Key, std::make_any<bool>(loop));
+    args.insertOrAssign(ReplaceElementAttributesWithNeighborValuesFilter::k_ComparisonDataPath, std::make_any<DataPath>(k_DataPath));
+    args.insertOrAssign(ReplaceElementAttributesWithNeighborValuesFilter::k_SelectedImageGeometryPath_Key, std::make_any<DataPath>(DataPath({k_ImageGeometry})));
+
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+  }
+}
+
+TEST_CASE("SimplnxCore::ReplaceElementAttributesWithNeighborValuesFilter: No Confidence", "[SimplnxCore][ReplaceElementAttributesWithNeighborValuesFilter]")
+{
+  UnitTest::LoadPlugins();
+
+  const float32 confidence = 0.9f;
+  const bool loop = false;
+  const uint64 comparison = 0; // Less than
+  DataStructure dataStructure = CreateTestData();
+  RunFilter(dataStructure, comparison, confidence, loop);
+  CheckTest1Output(dataStructure);
+}
+
+TEST_CASE("SimplnxCore::ReplaceElementAttributesWithNeighborValuesFilter: No Loop: Less Than", "[SimplnxCore][ReplaceElementAttributesWithNeighborValuesFilter]")
+{
+  UnitTest::LoadPlugins();
+
+  const float32 confidence = 0.005f;
+  const bool loop = false;
+  const uint64 comparison = 0;
+  DataStructure dataStructure = CreateTestData();
+  RunFilter(dataStructure, comparison, confidence, loop);
+  CheckTest2Output(dataStructure);
+}
+
+TEST_CASE("SimplnxCore::ReplaceElementAttributesWithNeighborValuesFilter: No Loop: Greater Than", "[SimplnxCore][ReplaceElementAttributesWithNeighborValuesFilter]")
+{
+  UnitTest::LoadPlugins();
+
+  const float32 confidence = 0.005f;
+  const bool loop = false;
+  const uint64 comparison = 1; // Greater than
+  DataStructure dataStructure = CreateTestData();
+  RunFilter(dataStructure, comparison, confidence, loop);
+  CheckTest3Output(dataStructure);
+}
+
+TEST_CASE("SimplnxCore::ReplaceElementAttributesWithNeighborValuesFilter: Loop: Less Than", "[SimplnxCore][ReplaceElementAttributesWithNeighborValuesFilter]")
+{
+  UnitTest::LoadPlugins();
+
+  const float32 confidence = 0.005f;
+  const bool loop = true;
+  const uint64 comparison = 0;
+  DataStructure dataStructure = CreateTestData();
+  RunFilter(dataStructure, comparison, confidence, loop);
+  CheckTest4Output(dataStructure);
+}
+
+TEST_CASE("SimplnxCore::ReplaceElementAttributesWithNeighborValuesFilter: Loop: Greater Than", "[SimplnxCore][ReplaceElementAttributesWithNeighborValuesFilter]")
+{
+  UnitTest::LoadPlugins();
+
+  const float32 confidence = 0.005f;
+  const bool loop = true;
+  const uint64 comparison = 1;
+  DataStructure dataStructure = CreateTestData();
+  RunFilter(dataStructure, comparison, confidence, loop);
+  CheckTest5Output(dataStructure);
+}
+
+TEST_CASE("SimplnxCore::ReplaceElementAttributesWithNeighborValuesFilter: Loop: Less Than v2", "[SimplnxCore][ReplaceElementAttributesWithNeighborValuesFilter]")
+{
+  UnitTest::LoadPlugins();
+
+  const float32 confidence = 0.01f;
+  const bool loop = true;
+  const uint64 comparison = 0;
+  DataStructure dataStructure = CreateTestData();
+  RunFilter(dataStructure, comparison, confidence, loop);
+  CheckTest6Output(dataStructure);
+}
+
+TEST_CASE("SimplnxCore::ReplaceElementAttributesWithNeighborValuesFilter: Loop: Greater Than v2", "[SimplnxCore][ReplaceElementAttributesWithNeighborValuesFilter]")
+{
+  UnitTest::LoadPlugins();
+
+  const float32 confidence = 0.01f;
+  const bool loop = true;
+  const uint64 comparison = 1;
+  DataStructure dataStructure = CreateTestData();
+  RunFilter(dataStructure, comparison, confidence, loop);
+  CheckTest7Output(dataStructure);
+}
 
 TEST_CASE("SimplnxCore::ReplaceElementAttributesWithNeighborValuesFilter", "[SimplnxCore][ReplaceElementAttributesWithNeighborValuesFilter]")
 {
