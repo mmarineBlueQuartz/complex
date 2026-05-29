debc8a637edc1b77bcc0ec8871bb0b503f459c5f
Matthew Marine <matthew.marine@bluequartz.net>
2026-05-29 12:57:38 -0400

    Refactor ReplaceElementAttributeWithNeighborValues

--- src/Plugins/SimplnxCore/src/SimplnxCore/Filters/Algorithms/ReplaceElementAttributesWithNeighborValues.cpp
+++ src/Plugins/SimplnxCore/src/SimplnxCore/Filters/Algorithms/ReplaceElementAttributesWithNeighborValues.cpp
@@ -92,13 +92,121 @@ struct ExecuteTemplate
     }
   }
 
+  /**
+   * @brief
+   * @param
+   * @return Returns the number of voxels to be changed.
+   */
+  template <typename T>
+  usize findBestNeighbors(const std::array<int64, 3>& dims, const AbstractDataStore<T>& inputStore, std::shared_ptr<IComparisonFunctor<T>> comparator, float32 thresholdValue,
+                          const std::array<int64, 6>& neighborVoxelIndexOffsets, std::vector<int64_t>& bestNeighbor, int64& prog, int64 progIncrement,
+                          const IFilter::MessageHandler& messageHandler)
+  {
+    const usize totalPoints = inputStore.getNumberOfTuples();
+    int64 neighbor = 0;
+    usize count = 0;
+
+    int64 column = 0;
+    int64 row = 0;
+    int64 plane = 0;
+
+    // Iterate over the voxels and compare neighbors
+    for(usize voxelIndex = 0; voxelIndex < totalPoints; voxelIndex++)
+    {
+      if(comparator->compare(inputStore[voxelIndex], thresholdValue))
+      {
+        column = voxelIndex % dims[0];
+        row = (voxelIndex / dims[0]) % dims[1];
+        plane = voxelIndex / (dims[0] * dims[1]);
+        count++;
+        float32 best = inputStore[voxelIndex];
+
+        neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[0];
+        if(plane != 0)
+        {
+          CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
+        }
+        neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[1];
+        if(row != 0)
+        {
+          CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
+        }
+        neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[2];
+        if(column != 0)
+        {
+          CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
+        }
+        neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[3];
+        if(column != (dims[0] - 1))
+        {
+          CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
+        }
+        neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[4];
+        if(row != (dims[1] - 1))
+        {
+          CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
+        }
+        neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[5];
+        if(plane != (dims[2] - 1))
+        {
+          CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
+        }
+      }
+      if(voxelIndex > prog)
+      {
+        int64 progressInt = static_cast<int64>(((float)voxelIndex / totalPoints) * 100.0f);
+        const std::string progressMessage = fmt::format("Processing Loop({}) Progress: {}% Complete", count, progressInt);
+        messageHandler(IFilter::ProgressMessage{IFilter::Message::Type::Progress, progressMessage, static_cast<int32_t>(progressInt)});
+        prog += progIncrement;
+      }
+    }
+
+    return count;
+  }
+
+  /**
+  * @brief 
+  */
+  void replaceAttributesWithNeighbor(const usize totalPoints, const std::vector<int64>& bestNeighbor, int64& prog, int64 count, const AttributeMatrix& attrMatrix,
+                                     const IFilter::MessageHandler& messageHandler)
+  {
+    // For each voxel, copy tuple values from the best neighbor
+    const int64 progIncrement = static_cast<int64>(totalPoints / 50);
+    //int64 prog = 1;
+    int64 progressInt = 0;
+    int64 neighbor;
+
+    for(int64 voxelIndex = 0; voxelIndex < totalPoints; voxelIndex++)
+    {
+      // Track and report progress
+      if(voxelIndex > prog)
+      {
+        progressInt = static_cast<int64>(((float)voxelIndex / totalPoints) * 100.0f);
+        const std::string progressMessage = fmt::format("Transferring Loop({}) Progress: {}% Complete", count, progressInt);
+        messageHandler(IFilter::ProgressMessage{IFilter::Message::Type::Progress, progressMessage, static_cast<int32_t>(progressInt)});
+        prog += progIncrement;
+      }
+
+      // Copies the best neighbor tuple
+      neighbor = bestNeighbor[voxelIndex];
+      if(neighbor != -1)
+      {
+        for(const auto& [dataId, dataObject] : attrMatrix)
+        {
+          auto& dataArray = dynamic_cast<IDataArray&>(*dataObject);
+          dataArray.copyTuple(neighbor, voxelIndex);
+        }
+      }
+    }
+  }
+
   template <typename T>
   void operator()(const ImageGeom& imageGeom, IDataArray* inputIDataArray, int32 comparisonAlgorithm, float thresholdValue, bool loopUntilDone, const std::atomic_bool& shouldCancel,
                   const IFilter::MessageHandler& messageHandler)
   {
     const auto& inputStore = inputIDataArray->template getIDataStoreRefAs<AbstractDataStore<T>>();
 
-    const size_t totalPoints = inputStore.getNumberOfTuples();
+    const usize totalPoints = inputStore.getNumberOfTuples();
 
     Vec3 udims = imageGeom.getDimensions();
     std::array<int64, 3> dims = {
@@ -107,117 +215,46 @@ struct ExecuteTemplate
         static_cast<int64>(udims[2]),
     };
 
-    // bool good = true;
-    int64 neighbor = 0;
-    int64 column = 0;
-    int64 row = 0;
-    int64 plane = 0;
-
-    constexpr FaceNeighborType k_NumFaceNeighbors = VoxelNeighbors<Image3D>::k_FaceNeighborCount;
-    const std::array<int64, k_NumFaceNeighbors> neighborVoxelIndexOffsets = initializeFaceNeighborOffsets(dims);
+    std::array<int64, 6> neighborVoxelIndexOffsets = initializeFaceNeighborOffsets(dims);
     std::vector<int64_t> bestNeighbor(totalPoints, -1);
 
-    size_t count = 0;
+    usize count = 0;
     bool keepGoing = true;
 
+    // Pick the appropriate comparison functor for the algorithm
     std::shared_ptr<IComparisonFunctor<T>> comparator = std::make_shared<LessThanComparison<T>>();
     if(comparisonAlgorithm == k_GreaterThanIndex)
     {
       comparator = std::make_shared<GreaterThanComparison<T>>();
     }
 
-    const AttributeMatrix* attrMatrix = imageGeom.getCellData();
+    // Get the Cell Data AttributeMatrix from the ImageGeom
+    // This is used to edit all of the arrays based on the best neighbor value.
+    const AttributeMatrix& attrMatrix = *imageGeom.getCellData();
 
+    // Loop until the algorithm stops finding changes to make
     while(keepGoing)
     {
       keepGoing = false;
-      count = 0;
       if(shouldCancel)
       {
         break;
       }
 
-      auto progIncrement = static_cast<int64_t>(totalPoints / 50);
+      auto progIncrement = static_cast<int64>(totalPoints / 50);
       int64 prog = 1;
-      int64 progressInt = 0;
-      for(size_t voxelIndex = 0; voxelIndex < totalPoints; voxelIndex++)
-      {
-        if(comparator->compare(inputStore[voxelIndex], thresholdValue))
-        {
-          column = voxelIndex % dims[0];
-          row = (voxelIndex / dims[0]) % dims[1];
-          plane = voxelIndex / (dims[0] * dims[1]);
-          count++;
-          float32 best = inputStore[voxelIndex];
 
-          neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[0];
-          if(plane != 0)
-          {
-            CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
-          }
-          neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[1];
-          if(row != 0)
-          {
-            CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
-          }
-          neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[2];
-          if(column != 0)
-          {
-            CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
-          }
-          neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[3];
-          if(column != (dims[0] - 1))
-          {
-            CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
-          }
-          neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[4];
-          if(row != (dims[1] - 1))
-          {
-            CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
-          }
-          neighbor = static_cast<int64>(voxelIndex) + neighborVoxelIndexOffsets[5];
-          if(plane != (dims[2] - 1))
-          {
-            CompareValues<T>(comparator, inputStore, neighbor, thresholdValue, best, bestNeighbor, voxelIndex);
-          }
-        }
-        if(voxelIndex > prog)
-        {
-          progressInt = static_cast<int64_t>(((float)voxelIndex / totalPoints) * 100.0f);
-          const std::string progressMessage = fmt::format("Processing Loop({}) Progress: {}% Complete", count, progressInt);
-          messageHandler(IFilter::ProgressMessage{IFilter::Message::Type::Progress, progressMessage, static_cast<int32_t>(progressInt)});
-          prog += progIncrement;
-        }
-      }
+      count = findBestNeighbors<T>(dims, inputStore, comparator, thresholdValue, neighborVoxelIndexOffsets, bestNeighbor, prog, progIncrement, messageHandler);
 
+      // Allow breaking out of the algorithm
       if(shouldCancel)
       {
         break;
       }
 
-      progIncrement = static_cast<int64_t>(totalPoints / 50);
-      prog = 1;
-      progressInt = 0;
-      for(int64 voxelIndex = 0; voxelIndex < totalPoints; voxelIndex++)
-      {
-        if(voxelIndex > prog)
-        {
-          progressInt = static_cast<int64_t>(((float)voxelIndex / totalPoints) * 100.0f);
-          const std::string progressMessage = fmt::format("Transferring Loop({}) Progress: {}% Complete", count, progressInt);
-          messageHandler(IFilter::ProgressMessage{IFilter::Message::Type::Progress, progressMessage, static_cast<int32_t>(progressInt)});
-          prog = prog + progIncrement;
-        }
+      replaceAttributesWithNeighbor(totalPoints, bestNeighbor, prog, count, attrMatrix, messageHandler);
 
-        neighbor = bestNeighbor[voxelIndex];
-        if(neighbor != -1)
-        {
-          for(const auto& [dataId, dataObject] : *attrMatrix)
-          {
-            auto& dataArray = dynamic_cast<IDataArray&>(*dataObject);
-            dataArray.copyTuple(neighbor, voxelIndex);
-          }
-        }
-      }
+      // Check if the algorithm should continue looping
       if(loopUntilDone && count > 0)
       {
         keepGoing = true;
